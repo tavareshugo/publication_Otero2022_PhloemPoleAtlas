7cdb1e6feec344c8fe6121f2c141d71c19ee5f6b
Hugo Tavares <hm533@cam.ac.uk>
2022-04-25 17:34:02 +0100

    add analysis of CRISPR mutant repeat

--- analysis/ccc_phenotype.R
+++ analysis/ccc_phenotype.R
@@ -183,6 +183,225 @@ pdf("documents/pdf for figures/ccc_phenotype.pdf", width = 7.5, height = 8.5)
 (p1 / p2 / p3) + plot_annotation(tag_levels = "A")
 dev.off()
 
+
+
+# CRISPR mutant -----------------------------------------------------------
+
+crispr <- read_csv("data/raw/triple_mutant_phenotype/crispr_combined.csv")
+
+# bootstrapped average of each genotype
+resampled <- crispr %>%
+  mutate(strata = paste(condition, experiment, genotype, sep = ".")) %>%
+  select(genotype, condition, root_length, strata) %>%
+  bootstraps(times = 500) %>%
+  mutate(boot = map(splits, function(x){
+    dat <- as_tibble(x)
+    dat %>%
+      mutate(root_length = log(root_length)) %>%
+      group_by(strata, genotype, condition) %>%
+      summarise(mean_length = mean(root_length), .groups = "drop") %>%
+      group_by(genotype, condition) %>%
+      summarise(mean_length = mean(mean_length), .groups = "drop") %>%
+      ungroup() %>%
+      mutate(mean_length = exp(mean_length))
+  }))
+
+
+p1 <- crispr %>%
+  ggplot(aes(genotype, root_length)) +
+  geom_violin(scale = "width") +
+  ggbeeswarm::geom_quasirandom(alpha = 0.5) +
+  facet_grid(experiment ~ condition) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# average per genotype/condition
+p2 <- resampled %>%
+  unnest(boot) %>%
+  group_by(genotype, condition) %>%
+  summarise(mid = median(mean_length),
+            up = quantile(mean_length, 0.975),
+            lo = quantile(mean_length, 0.025)) %>%
+  ungroup() %>%
+  # mutate(genotype = fct_relevel(genotype, "wt", "wt_so679")) %>%
+  ggplot(aes(genotype, mid)) +
+  # geom_pointrange(stat = "summary", fun.data = median_hilow) +
+  geom_pointrange(aes(ymin = lo, ymax = up),
+                  position = position_dodge(width = 0.5)) +
+  facet_grid(~ condition) +
+  labs(x = "Genotype", y = "Root Length (mm)") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+p1 + ggtitle("Raw data distributions") +
+  p2 + ggtitle("Overall average per genotype (with 95% CI)") +
+  plot_layout(nrow = 2)
+
+library(lme4)
+
+# fit model
+fit <- lmer(log(root_length) ~ genotype + (1|experiment),
+            data = crispr %>% filter(condition == "0suc"))
+summary(fit)
+
+marginal_means <- emmeans::emmeans(fit, ~ genotype, type = "response") %>%
+  as_tibble()
+
+marginal_means %>%
+  mutate(genotype = fct_reorder(genotype, response)) %>%
+  ggplot(aes(genotype)) +
+  geom_pointrange(aes(y = response, ymin = lower.CL, ymax = upper.CL)) +
+  labs(x = "Genotype", y = "Root Length (mm)") +
+  coord_flip()
+
+
+# Both datasets ------------
+
+crispr <- read_csv("data/raw/triple_mutant_phenotype/crispr_combined.csv")
+
+# tidy to match ccc data
+crispr <- crispr %>%
+  mutate(genotype = ifelse(str_detect(genotype, "crispr"), str_replace(genotype, "_", "-"), genotype)) %>%
+  separate(genotype, c("genotype", "stock"), sep = "_") %>%
+  # mutate(stock = ifelse(is.na(stock), "new_stock", stock)) %>%
+  mutate(stock = case_when(is.na(stock) & genotype == "ccc" ~ "unknown",
+                           stock == "shared" & genotype == "ccc" ~ "shared_march21",
+                           is.na(stock) & genotype == "wt" ~ "so656",
+                           is.na(stock) ~ "new_stock",
+                           TRUE ~ stock)) %>%
+  select(genotype, root_length, stock, experiment_id = experiment, everything())
+
+# bind the two datasets together
+ccc_crispr <- crispr %>% filter(condition == "0suc") %>% select(-condition) %>%
+  bind_rows(ccc %>% select(genotype, root_length, stock, experiment_id)) %>%
+  # remove failed crispr mutant
+  filter(genotype != "crispr_63")
+
+# bootstrapped average of each genotype
+resampled <- ccc_crispr %>%
+  mutate(strata = paste(stock, experiment_id, genotype, sep = ".")) %>%
+  select(genotype, root_length, strata) %>%
+  bootstraps(times = 500) %>%
+  mutate(boot = map(splits, function(x){
+    dat <- as_tibble(x)
+    dat %>%
+      mutate(root_length = log(root_length)) %>%
+      group_by(strata, genotype) %>%
+      summarise(mean_length = median(root_length)) %>%
+      group_by(genotype) %>%
+      summarise(mean_length = median(mean_length)) %>%
+      ungroup() %>%
+      mutate(mean_length = exp(mean_length))
+  }))
+
+# average per genotype
+p1 <- resampled %>%
+  unnest(boot) %>%
+  group_by(genotype) %>%
+  summarise(mid = median(mean_length),
+            up = quantile(mean_length, 0.975),
+            lo = quantile(mean_length, 0.025)) %>%
+  ungroup() %>%
+  mutate(genotype = fct_reorder(genotype, mid)) %>%
+  ggplot(aes(genotype, mid)) +
+  # geom_pointrange(stat = "summary", fun.data = median_hilow) +
+  geom_pointrange(aes(ymin = lo, ymax = up)) +
+  labs(x = "Genotype", y = "Root Length (mm)") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# variation across experiments
+p2 <- ccc_crispr %>%
+  filter(genotype %in% c("ccc", "wt", "cdf2", "cog1-6cdf4", "crispr-61", "crispr-66")) %>%
+  mutate(genotype = factor(genotype, levels = c("ccc",
+                                             "crispr-66",
+                                             "wt",
+                                             "crispr-61",
+                                             "cog1-6cdf4",
+                                             "cdf2"))) %>%
+  mutate(stock = fct_relevel(stock, "so488", "original")) %>%
+  ggplot(aes(stock, root_length)) +
+  geom_pointrange(stat = "summary", fun.data = mean_cl_boot,
+                  aes(group = interaction(stock, experiment_id, genotype),
+                      colour = experiment_id),
+                  position = position_dodge(width = 0.7)) +
+  facet_grid( ~ genotype, scales = "free_x", space = "free_x") +
+  ggthemes::scale_colour_tableau("Classic Green-Orange 12") +
+  theme_classic(base_size = 14) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Seed stock", y = "Root Length (mm)", colour = "Experiment")
+
+pdf("documents/pdf for figures/ccc_crispr_phenotype.pdf", width = 10, height = 8.5)
+(p1 / p2) + plot_annotation(tag_levels = "A")
+dev.off()
+
+
+# Repeat experiment -------------
+
+ccc_repeat <- read_csv("data/raw/triple_mutant_phenotype/phloem_mutants_Rfile.csv")
+
+resampled <- ccc_repeat %>%
+  # filter(sucrose == "zero") %>%
+  mutate(strata = paste(sucrose, replicate, genotype, sep = ".")) %>%
+  select(genotype, sucrose, root_length, strata) %>%
+  bootstraps(times = 500) %>%
+  mutate(boot = map(splits, function(x){
+    dat <- as_tibble(x)
+    dat %>%
+      mutate(root_length = log(root_length)) %>%
+      group_by(strata, genotype, sucrose) %>%
+      summarise(mean_length = median(root_length)) %>%
+      group_by(genotype, sucrose) %>%
+      summarise(mean_length = median(mean_length)) %>%
+      ungroup() %>%
+      mutate(mean_length = exp(mean_length))
+  }))
+
+# average per genotype
+p1 <- resampled %>%
+  unnest(boot) %>%
+  group_by(genotype, sucrose) %>%
+  summarise(mid = median(mean_length),
+            up = quantile(mean_length, 0.975),
+            lo = quantile(mean_length, 0.025)) %>%
+  ungroup() %>%
+  mutate(genotype = factor(genotype, levels = c("wt",
+                                                "3papl",
+                                                "3papl-2",
+                                                "apl",
+                                                "p1p2",
+                                                "6se")),
+         sucrose = factor(sucrose, levels = c("sucrose", "zero"))) %>%
+  ggplot(aes(genotype, mid, colour = sucrose)) +
+  # geom_pointrange(stat = "summary", fun.data = median_hilow) +
+  geom_pointrange(aes(ymin = lo, ymax = up),
+                  position = position_dodge(0.5)) +
+  labs(x = "Genotype", y = "Root Length (mm)", colour = "Sucrose") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  coord_cartesian(ylim  = c(0, 3))
+
+# variation across experiments
+p2 <- ccc_repeat %>%
+  mutate(genotype = factor(genotype, levels = c("wt",
+                                                "3papl",
+                                                "3papl-2",
+                                                "apl",
+                                                "p1p2",
+                                                "6se")),
+         sucrose = factor(sucrose, levels = c("sucrose", "zero"))) %>%
+  ggplot(aes(replicate, root_length)) +
+  geom_pointrange(stat = "summary", fun.data = mean_cl_boot,
+                  aes(group = interaction(replicate, genotype, sucrose),
+                      shape = replicate, colour = sucrose),
+                  position = position_dodge(width = 0.5)) +
+  facet_grid( ~ genotype, scales = "free_x", space = "free_x") +
+  theme_classic(base_size = 14) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Replicate", y = "Root Length (mm)", colour = "Sucrose") +
+  coord_cartesian(ylim  = c(0, 3))
+
+pdf("documents/pdf for figures/ccc_crispr_phenotype_repeat.pdf", width = 10, height = 8.5)
+(p1 / p2) + plot_annotation(tag_levels = "A")
+dev.off()
+
+
 # Deprecated ---------------
 library(rsample)
 
